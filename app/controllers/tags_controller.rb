2415223d1f69197740dc4eddc1298fcf9f9515ea
lisaylin <lin.lisa@hotmail.com>
2015-12-12 10:34:19 -0500

    Ready for Heroku

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,14 @@
+class TagsController < ApplicationController
+	before_filter :require_login, only: [:destroy]
+		
+	def show
+		@tag = Tag.find(params[:id])
+	end
+	
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+		flash.notice = "Tag '#{@tag.name}' Deleted!"
+		redirect_to action: "index"
+	end
+end
